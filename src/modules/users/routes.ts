77072bd748a15d4a317693c20b60c62e72fd3ad8
FedericoHerreraa <fede.juan.herrera@gmail.com>
2025-08-14 11:17:20 -0300

    agregando mas rutas en /users y dos funciones en el controller

--- src/modules/users/routes.ts
+++ src/modules/users/routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import { authMiddleware } from '../../middleware/authMiddleware';
-import { getUserProfile } from './controller';
+import { getUser, getUsers, getUserProfile } from './controller';
 
 const router = Router();
 
 router.get('/me', authMiddleware, getUserProfile);
+
+router.get('/:id', getUser);
+router.get('/', getUsers);
 
 export default router;
