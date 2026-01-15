3348fe43c32ab7f2a5b021c0d536f76621673910
marielboher1 <mariel.boher@capasso.tech>
2026-01-15 16:57:33 -0300

    mostrar ebooks y eventos

--- src/modules/events/routes.ts
+++ src/modules/events/routes.ts
@@ -1,17 +1,48 @@
 import { Router } from "express";
-import { createEvent, updateEvent, deleteEvent, getAllEvents, getEventById, getEventInscripciones } from "./controller";
-import { AuthenticatedRequest, authMiddleware } from "../../middleware/authMiddleware";
+import {
+  createEvent,
+  updateEvent,
+  deleteEvent,
+  getAllEvents,
+  getEventById,
+  getEventInscripciones,
+  getUserEvents,
+} from "./controller";
+import {
+  AuthenticatedRequest,
+  authMiddleware,
+} from "../../middleware/authMiddleware";
 import { Request, Response } from "express";
-import { basicSanitization, validateBody } from "../../middleware/zodValidation";
+import {
+  basicSanitization,
+  validateBody,
+} from "../../middleware/zodValidation";
 import { EventCreateSchema, EventUpdateSchema } from "../../types/events";
-
 const router = Router();
-
-router.get('/', getAllEvents);
-router.get('/:eventId/inscripciones', authMiddleware, (req: Request, res: Response) => getEventInscripciones(req as AuthenticatedRequest, res));
-router.get('/:id', getEventById);
-router.post('/', authMiddleware, basicSanitization, validateBody(EventCreateSchema), (req: Request, res: Response) => createEvent(req as AuthenticatedRequest, res));
-router.put('/:id', authMiddleware, basicSanitization, validateBody(EventUpdateSchema), (req: Request, res: Response) => updateEvent(req as AuthenticatedRequest, res));
-router.delete('/:id', authMiddleware, (req: Request, res: Response) => deleteEvent(req as AuthenticatedRequest, res));
-
+router.get("/", getAllEvents);
+router.get("/user/:id", getUserEvents);
+router.get(
+  "/:eventId/inscripciones",
+  authMiddleware,
+  (req: Request, res: Response) =>
+    getEventInscripciones(req as AuthenticatedRequest, res)
+);
+router.get("/:id", getEventById);
+router.post(
+  "/",
+  authMiddleware,
+  basicSanitization,
+  validateBody(EventCreateSchema),
+  (req: Request, res: Response) => createEvent(req as AuthenticatedRequest, res)
+);
+router.put(
+  "/:id",
+  authMiddleware,
+  basicSanitization,
+  validateBody(EventUpdateSchema),
+  (req: Request, res: Response) => updateEvent(req as AuthenticatedRequest, res)
+);
+router.delete("/:id", authMiddleware, (req: Request, res: Response) =>
+  deleteEvent(req as AuthenticatedRequest, res)
+);
 export default router;
