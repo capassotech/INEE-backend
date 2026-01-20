4de99605ef047c67a3e3ec510c1009859fa22c98
FedericoHerreraa <fede.juan.herrera@gmail.com>
2026-01-20 15:53:06 -0300

    eliminando todo lo relacionado con checkout API

--- src/modules/payments/routes.ts
+++ src/modules/payments/routes.ts
@@ -1,15 +1,12 @@
 import { Router } from "express";
-import { createPayment, handleWebhook, createPreference } from "./controller";
+import { handleWebhook, createPreference } from "./controller";
 
 const router = Router();
-
-// Checkout API con tarjeta en sitio
-router.post("/mercadopago/create-payment", createPayment);
 
 // Checkout PRO (preferencias y redirección a Mercado Pago)
 router.post("/mercadopago/create-preference", createPreference);
 
-// Webhook común para ambos flujos
+// Webhook para Checkout PRO
 router.post("/mercadopago/webhook", handleWebhook);
 
 export default router;
