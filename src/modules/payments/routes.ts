4dffa3e8047ceb7a9f2d6142f2660a571211f6cd
eliascapasso <elias_capasso@live.com>
2026-01-23 10:43:02 -0300

    merge

--- src/modules/payments/routes.ts
+++ src/modules/payments/routes.ts
@@ -4,17 +4,7 @@ import { createPayment, handleWebhook, createPreference } from "./controller";
 const router = Router();
 
 router.post("/mercadopago/create-payment", createPayment);
-import { handleWebhook, createPreference } from "./controller";
-
-const router = Router();
-
 router.post("/mercadopago/create-preference", createPreference);
-
 router.post("/mercadopago/webhook", handleWebhook);
-
-// Implementacion de Checkout API oculta
-
-// router.post("/mercadopago/create-payment", createPayment);
-
 
 export default router;
