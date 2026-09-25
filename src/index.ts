d538d58a5bdf360cd05807bc2385c04dd9182297
FedericoHerreraa <fede.juan.herrera@gmail.com>
2026-09-25 16:56:31 -0300

    solucionando error recaptcha

--- src/index.ts
+++ src/index.ts
@@ -42,7 +42,6 @@ import { restoreActivePaypalProofReminders } from "./modules/recordatorios/contr
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.set("trust proxy", 1);
 app.use(cors());
 app.use(express.json());
 
